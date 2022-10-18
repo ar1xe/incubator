f353702a96c1cc79af91a1b06d31cfbcacbc75df
ar1xe <chelseagrin52@gmail.com>
2022-10-18 22:44:43 +0300

    continuation of the solution js tasks

---
+++ js/038.js
@@ -0,0 +1,16 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+  "qwdqwd " +
+  myNoun +
+  " qwd zxzxc " +
+  myAdjective +
+  " asdqqq " +
+  myVerb +
+  " czxc23s " +
+  myAdverb; // Change this line
+// Only change code above this line
